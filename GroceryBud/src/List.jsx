8ee7ff2cc1c597e49e805503081ecec5366191f2
Aloysius Beronque <aloysiusmatthew1@gmail.com>
2022-11-10 15:07:07 +0800

    Created app structure

---
+++ GroceryBud/src/List.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function List() {
+    return(
+        <div>
+          <article className="article-container">
+            <p>Eggs</p>
+            <div>
+              <button>Edit</button>
+              <button>Delete</button>
+            </div>
+          </article>
+        </div>)
+}
+
+export default List;
